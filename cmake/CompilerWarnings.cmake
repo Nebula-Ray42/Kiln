a811fb56dd72bd010f34fd862197216c788e8fae
Emukei555 <emukei007@gmail.com>
2026-08-06 14:06:45 +0900

    Changes to the Project Structure

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -1,5 +1,5 @@
-function(set_project_warnings project_name)
-    target_compile_options(${project_name} PRIVATE
+function(set_project_warnings kiln)
+    target_compile_options(${kiln} PRIVATE
             -Wall
             -Wextra
             -Wpedantic
